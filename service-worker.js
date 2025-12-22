bce3ed728e87cf2b2a63b79393f3e1791aba545b
nanithenanny <133074694+nanithenanny@users.noreply.github.com>
2025-12-22 10:20:17 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "stockup-v1";
+const CACHE = "stockup-v3";
 const ASSETS = ["./", "./index.html", "./style.css", "./app.js", "./manifest.json"];
 
 self.addEventListener("install", (e) => {
